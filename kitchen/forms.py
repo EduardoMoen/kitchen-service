151d0e48744257299dc667d3e2adf21f1329fcd1
Eduardo Doerner <eduardodoerner@gmail.com>
2025-12-29 18:29:06 -0300

    add search to dish list

--- kitchen/forms.py
+++ kitchen/forms.py
@@ -25,3 +25,17 @@ class DishTypesSearchForm(forms.Form):
             }
         )
     )
+
+
+class DishSearchForm(forms.Form):
+    name = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "Search by name",
+                "class": "form-control",
+            }
+        )
+    )
--- kitchen/views.py
+++ kitchen/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
 
-from .forms import CookCreateForm, DishTypesSearchForm
+from .forms import CookCreateForm, DishTypesSearchForm, DishSearchForm
 from .models import Cook, Dish, DishType
 
 def index(request):
@@ -71,6 +71,28 @@ class DishTypeDeleteView(generic.DeleteView):
 
 class DishListView(generic.ListView):
     model = Dish
+
+    def get_context_data(
+        self, *, object_list = ..., **kwargs
+    ):
+        context = super(DishListView, self).get_context_data(**kwargs)
+
+        name = self.request.GET.get("name", "")
+
+        context["search_form"] = DishSearchForm(
+            initial={"name": name}
+        )
+
+        return context
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        name = self.request.GET.get("name")
+
+        if name:
+            queryset = queryset.filter(name__icontains=name)
+
+        return queryset
 
 
 class DishDetailView(generic.DetailView):
